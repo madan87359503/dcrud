89f35ec7318d24d8bb263b4ec6852ecffd97f28f
Madan <madan.actnepal.com.np>
2023-04-17 17:50:40 +0545

    aaa

--- src/views/website/T892343505/head.blade.php
+++ src/views/website/T892343505/head.blade.php
@@ -4,7 +4,6 @@
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Favicon -->
     <link href="/vendors/favicon.ico" rel="icon">
 
