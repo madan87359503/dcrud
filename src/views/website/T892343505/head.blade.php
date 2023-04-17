03ca362154480a49504ee5f62ae4cc19b24e0b21
Madan <madan.actnepal.com.np>
2023-04-17 16:11:46 +0545

    website assets added

--- src/views/website/T892343505/head.blade.php
+++ src/views/website/T892343505/head.blade.php
@@ -6,7 +6,7 @@
     <meta content="" name="description">
 
     <!-- Favicon -->
-    <link href="{{url("/")}}/templates/892343505/seo-agency-website-template/img/favicon.ico" rel="icon">
+    <link href="{{url("/")}}/vendors/favicon.ico" rel="icon">
 
     <!-- Google Web Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -18,13 +18,13 @@
     <href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
 
     <!-- Libraries Stylesheet -->
-    <link href="{{url("/")}}/templates/892343505/seo-agency-website-template/lib/animate/animate.min.css" rel="stylesheet">
-    <link href="{{url("/")}}/templates/892343505/seo-agency-website-template/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
-    <link href="{{url("/")}}/templates/892343505/seo-agency-website-template/lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+    <link href="{{url("/")}}/vendors/892343505/lib/animate/animate.min.css" rel="stylesheet">
+    <link href="{{url("/")}}/vendors/892343505/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="{{url("/")}}/vendors/892343505/lib/lightbox/css/lightbox.min.css" rel="stylesheet">
 
     <!-- Customized Bootstrap Stylesheet -->
-    <link href="{{url("/")}}/templates/892343505/seo-agency-website-template/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{url("/")}}/vendors/892343505/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Template Stylesheet -->
-    <link href="{{url("/")}}/templates/892343505/seo-agency-website-template/css/style.css" rel="stylesheet">
+    <link href="{{url("/")}}/vendors/892343505/css/style.css" rel="stylesheet">
 </head>
--- src/views/website/T892343505/js.blade.php
+++ src/views/website/T892343505/js.blade.php
@@ -2,12 +2,12 @@
     <!-- JavaScript Libraries -->
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="{{url("/")}}/templates/731679811/seo-agency-website-template/lib/wow/wow.min.js"></script>
-    <script src="{{url("/")}}/templates/731679811/seo-agency-website-template/lib/easing/easing.min.js"></script>
-    <script src="{{url("/")}}/templates/731679811/seo-agency-website-template/lib/waypoints/waypoints.min.js"></script>
-    <script src="{{url("/")}}/templates/731679811/seo-agency-website-template/lib/owlcarousel/owl.carousel.min.js"></script>
-    <script src="{{url("/")}}/templates/731679811/seo-agency-website-template/lib/isotope/isotope.pkgd.min.js"></script>
-    <script src="{{url("/")}}/templates/731679811/seo-agency-website-template/lib/lightbox/js/lightbox.min.js"></script>
+    <script src="{{url("/")}}/vendors/892343505/lib/wow/wow.min.js"></script>
+    <script src="{{url("/")}}/vendors/892343505/lib/easing/easing.min.js"></script>
+    <script src="{{url("/")}}/vendors/892343505/lib/waypoints/waypoints.min.js"></script>
+    <script src="{{url("/")}}/vendors/892343505/lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="{{url("/")}}/vendors/892343505/lib/isotope/isotope.pkgd.min.js"></script>
+    <script src="{{url("/")}}/vendors/892343505/lib/lightbox/js/lightbox.min.js"></script>
 
     <!-- Template Javascript -->
-    <script src="{{url("/")}}/templates/731679811/seo-agency-website-template/js/main.js"></script>
+    <script src="{{url("/")}}/vendors/892343505/js/main.js"></script>
