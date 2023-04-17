1365def657f5261d5448e59a49252275ee9b37d0
Madan <madan.actnepal.com.np>
2023-04-17 17:48:26 +0545

    viewport added to webpage

--- src/views/website/T892343505/head.blade.php
+++ src/views/website/T892343505/head.blade.php
@@ -4,9 +4,9 @@
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
-
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Favicon -->
-    <link href="{{url("/")}}/vendors/favicon.ico" rel="icon">
+    <link href="/vendors/favicon.ico" rel="icon">
 
     <!-- Google Web Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
