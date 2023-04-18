6073af2824e45882f32b8a73618ed3f7db4dd1e6
Madan <madan.actnepal.com.np>
2023-04-18 10:53:17 +0545

    aaaaaaaaaa#aaaaa

--- src/views/website/T892343505/body.blade.php
+++ src/views/website/T892343505/body.blade.php
@@ -9,7 +9,7 @@
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
                     <span class="fa fa-bars"></span>
-                </button>s
+                </button>
                 <div class="collapse navbar-collapse" id="navbarCollapse">
                     <div class="navbar-nav ms-auto py-0">
 @foreach($navpanel as $n)                        
